422f17af0e425a56a904d9085938a003e75f869c
AlucardxTepes <alucard.557@gmail.com>
2017-05-11 17:02:39 -0400

    Add nav bar component

--- bookstore-angular/admin-portal/src/app/app.module.ts
+++ bookstore-angular/admin-portal/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'hammerjs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule, MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
