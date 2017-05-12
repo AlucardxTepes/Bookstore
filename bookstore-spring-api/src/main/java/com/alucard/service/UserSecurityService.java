615bb28e78c375ef38423342b42257e4d207d2da
AlucardxTepes <alucard.557@gmail.com>
2017-05-12 01:49:30 -0400

    Fix crash by adding missing @Service annotation to UserSecurityService class

--- bookstore-spring-api/src/main/java/com/alucard/service/UserSecurityService.java
+++ bookstore-spring-api/src/main/java/com/alucard/service/UserSecurityService.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Alucard on 12-May-17.
  */
+@Service
 public class UserSecurityService implements UserDetailsService {
 
   private static final Logger log = LoggerFactory.getLogger(UserSecurityService.class);
